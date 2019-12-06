4f417e71fa531a28c3b18ddf0f626982b7e1b84c
Nettly_ <lyons.safyre@gmail.com>
2019-12-06 15:13:36 -0700

    Create install-nodejs12.sh

---
+++ Debian/install-nodejs12.sh
@@ -0,0 +1,14 @@
+if command -v curl >/dev/null 2>&1 ; then
+    echo "curl found"
+    sudo apt -y update && sudo apt -y upgrade
+    curl -sL https://deb.nodesource.com/setup_12.x | sudo bash -
+    sudo apt-get install -y nodejs
+    node -v
+else
+    echo "curl not found, finding curl"
+    sudo apt install curl
+    sudo apt -y update && sudo apt -y upgrade
+    curl -sL https://deb.nodesource.com/setup_12.x | sudo bash -
+    sudo apt-get install -y nodejs
+    node -v
+fi
